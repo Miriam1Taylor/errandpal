b3e0be9028d75ba8d88c993ccc010cf8b844d518
Miriam1Taylor <1300271325@qq.com>
2025-04-26 12:35:27 +0800

    solved:excel图表下载

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -208,6 +208,7 @@ public class ReportServiceImpl implements ReportService {
         LocalDate end = LocalDate.now().minusDays(1);
         //查询概览运营数据，提供给Excel模板文件
         BusinessDataVO businessData = workspaceService.getBusinessData(LocalDateTime.of(begin,LocalTime.MIN), LocalDateTime.of(end, LocalTime.MAX));
+        //数据在resources/template
         InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("template/运营数据报表模板.xlsx");
         try {
             //基于提供好的模板文件创建一个新的Excel表格对象
